1cecd36e7705d6c1824d8e97efea1048d026b113
josymadamana <josymadamana@gmail.com>
2017-09-17 19:52:12 -0400

    Update hellowordcount.py

--- hellowordcount.py
+++ hellowordcount.py
@@ -1,7 +1,11 @@
 ## Open file and cound the number of occurance of the word "Hello" without any case check##
+foundFlag=0
 file=open("file.txt","r")
 for word in file.read().split():
     if(word.lower() == 'hello'):
-        print "YES"
-    else
-        print "NO"
+	foundFlag=1
+	break
+if(foundFlag):
+    print "YES"
+else:
+    print "NO"
